0d671e6f6f8a9f7aa4f2c62b5ee3a755244f58e0
Eugene <lin.eugene.l.e@gmail.com>
2023-09-24 17:40:24 -0500

    Add mongodb extension

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const querystring = require('node:querystring');
+require('dotenv').config();
 
-var dt = require('./modules/dateModule')
+const { MongoClient } = require("mongodb");
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let clientPromise;
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    const client = new MongoClient(uri, options);
+    clientPromise = client.connect().then((connectedClient) => {
+      global._mongoClientPromise = connectedClient;
+      return connectedClient;
+    });
+  } else {
+    clientPromise = Promise.resolve(global._mongoClientPromise);
+  }
+} else {
+  const client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
 
 
 const route = {
@@ -19,11 +44,30 @@ const route = {
         const filePath = path.join(__dirname, 'public', 'participant.html')
         _staticFileHandler(filePath, res);
     },
+    '/create-room': (req, res, parameters) => {
+        console.log('create-room');
+        console.log(req.body);
+        // create a room check if it already exists in the database
+        // if it does not exist, initialize a document for it
+        // if it does exist, return return previous questions and response
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('create-room');
+    },
+    '/test': (req, res, parameters) => {
+        clientPromise.then((client) => {
+            const db = client.db('test');
+            const collection = db.collection('test');
+            collection.insertOne({test: 'test'});
+        })
+        console.log('test');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('test');
+    }
 }
 
 const middleware = [
     (req, res, next) => {
-        // console.log('middleware 1');
         next();
     },
     (req, res, next) => {
@@ -79,11 +123,9 @@ const server = http.createServer(function(req, res) {
     const contentType = getContentType(filePath);
     console.log(req.url); 
     var qstring = querystring.parse(req.url.split('?')[1]);
-    console.log(qstring)
-    console.log(qstring.room)
     req.url = req.url.split('?')[0];
     if(route[req.url]) {
-        route[req.url](req, res);
+        route[req.url](req, res, qstring);
         return;
     } else if(contentType !== 'text/html'){
         staticFileHandler(req, res);
